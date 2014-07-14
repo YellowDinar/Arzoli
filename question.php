6a10539609b1cce6095350dd1f451bd07fe9c6fb
KalimullinDinar <dinaritis@yandex.ru>
2014-07-14 21:07:25 +0400

    Все в php

---
+++ question.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Arzoli</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=1200px">
+    <link rel="stylesheet/less" type="text/css" href="css/style.less">
+    <link rel="stylesheet/less" type="text/css" href="css/question.less">
+    <script type="text/javascript" src="js/less.js"></script>
+    <!-- Put this script tag to the <head> of your page -->
+    <script type="text/javascript" src="http://vk.com/js/api/openapi.js?113"></script>
+    <script src="http://code.jquery.com/jquery-latest.min.js"
+        type="text/javascript"></script>
+    <script type="text/javascript">
+        VK.init({apiId: 4424623, onlyWidgets: true});
+    </script>
+
+    <!-- Put this div tag to the place, where the Comments block will be -->
+      
+    <style>
+        #question {
+            text-align: center;
+        }  
+    </style>
+      
+      <script>
+        $(function() {
+            var height = $(window).height();
+            var head = $('.div_header').height();
+            var foot = $('.div_footer').height();
+            var contentHeight = height-(head+foot);
+            var paddingContent = (50/contentHeight)*100;
+
+            $('.div_content').css('min-height', contentHeight);
+              
+            $('._message .btn_sim').click(function() {
+                $('._backForm').show();
+           });
+           $('._close a').click(function(){
+            $('._backForm').hide();
+           });
+        });
+      </script>
+      
+  </head>
+  <body>
+      <div class="_backForm">
+  <div class="_modalWindow">
+    <div class="_close">
+      <a href="javascript:">Закрыть</a>
+    </div>
+    <img src="/q/images/onmain.png" alt="">
+    <form action="modal_form.php" method="post">
+      <input type="hidden" name="page" id="page">
+      <script>
+          document.getElementById('page').value = window.location.href;
+      </script>
+      <div>
+      Имя: <br>
+      <input name="name" type="text">
+      </div>
+      <div>
+      Телефон или e-mail: <br>
+      <input name="tel" type="text">
+      </div>
+      <div>
+      Ваше Сообщение: <br>
+      <textarea name="question"></textarea>
+      </div>
+      <div>
+      <input class="btn_sim" type="submit" value="Отправить">
+      </div>
+    </form>
+  </div>
+</div>
+    <div class="div_header">
+      <div class="siteWidth">
+        <div class="_logo">
+          <a href="index.html"><img src="images/logo.png"></a>
+        </div>
+        <div class="_mainMenu">
+          <ul>
+            <li><a href="index.php">Главная</a></li>
+            <li><a href="about.php">О нас</a></li>
+            <li><a href="model.php">Модели</a></li>
+            <li><a href="news.php">Новости</a></li>
+            <li><a href="question.php">Вопрос/ответ</a></li>
+            <li><a href="contacts.php">Контакты</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="div_content">
+        <?php
+             if (strcasecmp($_GET["message"], "true") == 0) {
+                 echo "<h2 align=\"center\">Спасибо, за сообщение.</h2>";
+             }
+          ?>
+      <div class="siteWidth">
+        <h1>ВОПРОС-ОТВЕТ</h1> 
+        <div id="question" class="div_questions">
+            <div id="vk_comments"></div>
+            <script type="text/javascript">
+                VK.Widgets.Comments("vk_comments", {limit: 10, width: "520", attach: "*"});
+            </script>
+        </div>
+      </div>
+    </div>
+    <div class="div_footer">
+      <div class="siteWidth">
+        <div class="_contacts">
+          <div>Контакты</div>
+          <div>тел. 8-800-200-97-97 (Звонок бесплатный)</div>
+          <div>email: info@arzoli.ru</div>
+        </div>
+        <div class="_message">
+          <a href="javascript:" class="btn_sim">Оставьте сообщение</a>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
